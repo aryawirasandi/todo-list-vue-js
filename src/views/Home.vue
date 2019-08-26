38eb407f7c822de0005a349dc983fb2a3bef1180
Arya <aryawirasandi7153@gmail.com>
2019-08-26 23:50:45 +0700

    Add Card To Every Iteration of getters API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>
-      
-    </div>
+   <div>
+      <div v-for="(value, key) of todos" :key="key">
+      <v-card
+        class="mx-auto"
+      >
+        <v-card-title>{{value.title}}</v-card-title>
+        <v-card-text>{{value.body}}</v-card-text>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <v-card-actions>
+          <v-btn depressed color="primary">Update</v-btn>
+          <v-btn depressed color="error">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+      </div>
+   </div>
 </template>
 
 <script>
@@ -9,7 +22,7 @@ export default {
     name : "home",
     computed:{
       todos(){
-        return this.$store.getters.listTodos
+        return this.$store.getters.listTodos  
       }
     },
     created(){
