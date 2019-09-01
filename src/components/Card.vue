08ba99779b33eedfbbc2cdd9c2b94e0f33a1ab96
Arya <aryawirasandi7153@gmail.com>
2019-09-01 19:17:00 +0700

    Fill The Card Component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,27 @@
 <template>
     <div>
-        
-    </div>
+      <v-card
+        class="mx-auto"
+      >
+        <v-card-title>{{title}}</v-card-title>
+        <v-card-text>{{body}}</v-card-text>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <v-card-actions>
+          <v-btn depressed color="primary">Update</v-btn>
+          <v-btn depressed color="error">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+      </div>
 </template>
 <style>
 
 </style>
 <script>
 export default {
-    
+    props:{
+        title : String,
+        body : String
+    }
 }
 </script>
